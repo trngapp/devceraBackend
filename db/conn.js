45cf1f4311fcea2c0023bea87c270dbc2df20d23
trngapp <74776793+trngapp@users.noreply.github.com>
2023-04-04 19:15:26 +0530

    testing again the deployment

--- db/conn.js
+++ db/conn.js
@@ -1,10 +1,10 @@
-const mongoose=require("mongoose");
+/*const mongoose=require("mongoose");
 mongoose.connect("mongodb://localhost:27017/devcera",{useNewUrlParser:true,useUnifiedTopology:true})
 .then(()=>console.log("Connected..."))
-.catch((err)=>console.log(err));
+.catch((err)=>console.log(err));*/
 
-/*const DB= 'mongodb+srv://devcera21:E48znRlCth7D7skI@devcluster1.ukdjln3.mongodb.net/Devcera?retryWrites=true&w=majority';
+const DB= 'mongodb+srv://devcera21:E48znRlCth7D7skI@devcluster1.ukdjln3.mongodb.net/Devcera?retryWrites=true&w=majority';
 
 mongoose.connect(DB,{useNewUrlParser:true,useUnifiedTopology:true})
 .then(()=>console.log("Connected..."))
-.catch((err)=>console.log(err));*/
+.catch((err)=>console.log(err));
