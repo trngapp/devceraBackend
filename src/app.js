0392751c308943c57e81a1233247b6b67193fe31
trngapp <74776793+trngapp@users.noreply.github.com>
2023-04-04 18:43:19 +0530

    testing deployment

--- src/app.js
+++ src/app.js
@@ -1,50 +1,12 @@
-const express=require("express");
-const cookieParser=require("cookie-parser");
-const cors=require("cors");
-const router= new express.Router();
-const app=new express();
-require('../db/conn')
-app.use(express.json());
- User=require('../model/user');
-const userRouter=require("../router/userRouter");
-const applyRouter = require("../router/applyRouter");
-const createRouter = require("../router/createRouter");
+const express=require('express')
+const serverless=require('serverless-http')
+const app=express();
+const router=express.Router();
+router.get('/',(req,res)=>{
+    res.json({
+        'hello':'hi!'
+    });
+});
 
-
-
-const port =3336;
-
-app.use("/.netlify/functions/api",userRouter);
-app.use("/.netlify/functions/api",applyRouter);
-app.use("/.netlify/functions/api",createRouter);
-
-app.use(cookieParser());
-app.use(cors({credentials:true, origin:'http://localhost:3000'}));
-//app.use("/.netlify/functions/api",router);
-
-app.listen(port,()=>{
-   // console.log(process.env.TOKE_SECRET);
-    console.log("server started...");
-})
-/*app.post("/login", async (req,res)=>{
-    try {
-        const emai=req.body.email;
-       const  passwor=req.body.password;
-   const result = await User.findOne({$and:[{email:emai},{password:passwor}]});
-        if(result)
-        {
-            console.log("you are logged in successfully");
-            res.send("you are logged in successfully");
-
-        }
-        else{
-            console.log("either email or password is wrong");
-            res.send("either email or password is wrong");
-        }
-
-
-
-    } catch (error) {
-        res.status(400).send(error);
-    }
-})*/
+app.use('/.netlify/functions/api',router);
+module.exports.handler=serverless(app);
