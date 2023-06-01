5ee926dea86806b172b6dae128793468520dcf0c
trngapp <74776793+trngapp@users.noreply.github.com>
2023-06-02 02:03:08 +0530

    added email sending functionality in apply api

--- router/applyRouter.js
+++ router/applyRouter.js
@@ -10,7 +10,7 @@ const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
        auth: {
             user: 'tarangsharma1999@gmail.com',
-            pass: 'sharmaji',
+            pass: 'inyyubhgpdmbvzpw',
          },
     secure: true,
     });
